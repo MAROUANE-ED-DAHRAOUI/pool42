3ed1d3d3aab875a14768eb38ccac4fe505597349
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2023-09-22 16:29:13 +0000

    add file the print_alphabet

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+	write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
---
+++ C00/ex01/ft_print_alphabet_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+        write(1, "abcdefghijklmnopqrstuvwxyz", 26);
+}
+int main()
+{
+	ft_print_alphabet();
+	return(0);
+}
