cbfc67d370a5f37646241105174fd9046cb29241
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2023-09-24 14:56:00 +0000

    1337/med

---
+++ C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*int main()
+{
+	int n;
+	int *ptr8;
+	int **ptr7;
+	int ***ptr6;
+	int ****ptr5;
+	int *****ptr4;
+	int ******ptr3;
+	int *******ptr2;
+	int ********ptr1;
+	int *********ptr;
+
+	n = 20;
+
+	ptr8 = &n;
+	ptr7 = &ptr8;
+	ptr6 = &ptr7;
+	ptr5 = &ptr6;
+	ptr4 = &ptr5;
+	ptr3 = &ptr4;
+	ptr2 = &ptr3;
+	ptr1 = &ptr2;
+	ptr = &ptr1;
+
+	ft_ultimate_ft(ptr);
+
+	printf("%d\n", n);
+	
+	return (0);
+}*/
