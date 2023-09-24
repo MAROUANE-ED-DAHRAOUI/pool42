701555175432a0bb88d6271249b14b4aaec74822
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2023-09-24 14:10:45 +0000

    1337/med

---
+++ C01/ex00/ft_ft.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+/*int main()
+{
+	int *ptr;
+	int num;
+
+	ptr = &num;
+
+	ft_ft(ptr);
+	printf("%d\n", *ptr);
+
+	return (0);
+}*/
