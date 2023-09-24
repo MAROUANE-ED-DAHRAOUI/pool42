0da1908b77c25bc53ebcfeac5099bd9b67f51ad2
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2023-09-24 16:22:25 +0000

    1337/med

---
+++ C01/ex03/ft_div_mod.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	if (b != 0)
+	{
+		*div = a / b;
+		*mod = a % b;
+	}
+}
+
+/*int	main()
+{
+	int a;
+	int b;
+	int div;
+	int mod;
+
+	a = 100;
+	b = 10;
+
+	ft_div_mod(a, b, &div, &mod);
+
+	printf("%d\n", div);
+	printf("%d\n", mod);
+}*/
