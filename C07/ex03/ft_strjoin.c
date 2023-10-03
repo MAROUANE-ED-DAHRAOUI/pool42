636e14d65cabfda8731c75b36b0841ee4b6138ce
MAROUANE-ED-DAHRAOUI <eddahraouimarouane@gmail.com>
2023-10-03 23:28:42 +0000

    1337/med

--- C07/ex03/ft_strjoin.c
+++ C07/ex03/ft_strjoin.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int strlen(char *str)
+int ft_strlen(char *str)
 {
 	int i = 0;
-	while(str[i] != '\0')
+	while(str[i])
 	{
-		i++;
+		i++;	
 	}
 	return (i);
 }
+int full_lenght(int size, char **str, char sepr)
+{
+	int index = 0;
+	int len = 0;
 
-char *strcpy(char *dest, char *src)
+	if(size == 0)
+	{
+		return (0);
+	}
+
+	while(index < size)
+	{
+		len += ft_strlen(str[index]);
+		len += sepr;
+
+		index++;
+	}
+
+	len -= sepr;
+	return (len);
+}
+char *ft_strcpy(char *dest, char *src)
 {
 	int i = 0;
 	while(src[i] != '\0')
@@ -19,82 +39,61 @@ char *strcpy(char *dest, char *src)
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
 	return (dest);
 }
-
-int full_lenght(int size, char **str, int sep)
-{
-	int index;
-	int sumleng;
-
-	index = 0;
-	sumleng = 0;
-	while(index < size)
-	{
-		sumleng += strlen(str[index]);
-		sumleng += sep;
-		index++; 
-	}
-	sumleng -= sep;
-	return (sumleng);
-}
-
 char *ft_strjoin(int size, char **strs, char *sep)
 {
-	int index;
-	int len = 0;
-	char *str;
+	int len;
+	int index = 0;
+	char *ptr;
 	char *d;
 	
-	if(size == 0)
-		return ((char *)malloc(sizeof(char)));
+	len = 0;
 	
-	len = full_lenght(size, strs, strlen(sep));
-	d = (str = (char *)malloc((len + 1) * sizeof(char)));
+	len = full_lenght(size, strs, ft_strlen(sep));
+	d = (ptr = (char *)malloc(sizeof(char *) * len));
+	
+	if(size == 0)
+               return (malloc(sizeof(char)));
 
-	if(!d)
-	{
-		return (0);
-	}
-
-	index = 0;
 	while(index < size)
 	{
-		strcpy(d ,strs[index]);
-		d += strlen(strs[index]);
-
+		ft_strcpy(d, strs[index]);
+		d += ft_strlen(strs[index]);
+	
 		if(index < size - 1)
 		{
-			strcpy(d, sep);
-			d += strlen(sep);
+			ft_strcpy(d, sep);
+			d += ft_strlen(sep);
 		}
 		index++;
 	}
-	*d = '\0';
-	
-	return (str);
+	d = '\0';
 
+	return (ptr);
 }
 /*int main()
 {
 	char **arr;
 	char *ptr;
-	char *sep;
-	int size = 3;
-
-	arr = ((char**)malloc(size * sizeof(char *)));
+	char *sepr;
+	int size = 4;
+	
+	arr = malloc(sizeof(char*) * size);
 	arr[0] = malloc(sizeof(char *) * 5 + 1);
 	arr[1] = malloc(sizeof(char *) * 5 + 1);
-	arr[2] = malloc(sizeof(char *) * 13 + 1);
+	arr[2] = malloc(sizeof(char *) * 4 + 1);
+	arr[3] = malloc(sizeof(char *) * 10 + 1);
+	
 	arr[0] = "Hello";
-	arr[1] = "world";
-	arr[2] = "!this is good";
+	arr[1] = "World";
+	arr[2] = "Iam";
+	arr[3] = "in next st";
 
-	sep = " ";
-	ptr = ft_strjoin(size, arr, sep);
+	sepr = " ";
 
-	printf("%s", ptr);
-	printf("\n");
+	ptr = ft_strjoin(size, arr, sepr);
+	printf("%s\n", ptr);
+	
 	return (0);
 }*/
